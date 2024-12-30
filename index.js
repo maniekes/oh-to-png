cc3947d71a6f0fa1858d49e32abf9332d64e99b1
maniekes <maniekes@gmail.com>
2024-12-30 20:16:42 +0100

    another working version

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 import express from 'express';
 import Pageres from 'pageres';
 import stream from 'stream';
+import dotenv from 'dotenv';
+
+const env = process.env.NODE_ENV || 'production';
+dotenv.config({ path: `.env.${env}` });
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const OH_HOST = process.env.OH_HOST;
+const OH_RESOLUTION = process.env.OH_RESOLUTION || '960x540';
+const OH_KIOSK = process.env.OH_KIOSK || true;
+const dashboards = process.env.OH_ALLOWED_DASHBOARDS.split(',') || [];
+const kioskMode = OH_KIOSK ? '&kiosk=on' : '';
 
 app.get('/screenshot', async (req, res) => {
-    const url = req.query.url || 'https://example.com'; // URL to capture
-    const resolution = req.query.resolution || '960x540'; // Resolution
+    const resolution = req.query.resolution || OH_RESOLUTION;
+    if(!dashboards.includes(req.query.dashboard)) {
+        console.error(`dashboard ${req.query.dashboard} not in allowed list!`);
+        res.status(500).send('dashboard not in allowed list!');
+        return;
+    }
+    const url = `${OH_HOST}${req.query.dashboard}?${kioskMode}`
+    const delayTime = req.query.delay || 2;
 
     try {
         // Capture the screenshot with Pageres
-        const pageres = new Pageres({ delay: 2 })
+        const pageres = new Pageres({ delay: delayTime })
             .source(url, [resolution]);
 
         const screenshots = await pageres.run();
