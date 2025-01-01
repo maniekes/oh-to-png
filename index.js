884d6afb3314d951ed384b75f69dfb23ee7432ec
maniekes <maniekes@gmail.com>
2025-01-01 17:39:10 +0100

    added grayscale and negate

--- index.js
+++ index.js
@@ -1,42 +1,55 @@
 import express from 'express';
 import Pageres from 'pageres';
-import stream from 'stream';
 import fs from 'fs';
 import yaml from 'js-yaml';
+import sharp from 'sharp';
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 app.get('/screenshot', async (req, res) => {
     const fileContents = fs.readFileSync('./device-mappings.yaml', 'utf-8');
     const mappings = yaml.load(fileContents);
-    const device = mappings.devices[req.query.device];
-    if(!device) {
+    const foundDevice = mappings.devices[req.query.device];
+    if (!foundDevice) {
         console.error(`dashboard ${req.query.device} not in allowed list!`);
         res.status(500).send('device not found in configuration list!');
         return;
     }
-    const resolution = device.resolution || mappings.default.resolution || '960x540';
+    console.log(`device: ${JSON.stringify(foundDevice)}`);
+    const device = {...mappings.default, ...foundDevice};
+    console.log(`defaults: ${JSON.stringify(mappings.default)}`);
+    console.log(`device with defaults: ${JSON.stringify(device)}`);
+    const resolution = req.query.resolution || device.resolution;
+    const delayTime = req.query.delay || device.delay;
+    const negate = req.query.negate || device.negate;
+    const grayscale = req.query.grayscale || device.grayscale;
     const url = device.url;
-    const delayTime = device.delay || mappings.default.delay || 2;
+    console.log(`[${req.query.device}] fetching ${url} with resolution ${resolution}, delay ${delayTime}, negate ${negate}, grayscale ${grayscale}`);
+    console.log(`grayscale ${grayscale} ${typeof grayscale}`);
+    console.log(`negate ${negate} ${typeof negate}`);
     try {
         // Capture the screenshot with Pageres
         const pageres = new Pageres({ delay: delayTime })
             .source(url, [resolution]);
 
         const screenshots = await pageres.run();
-
         if (screenshots.length > 0) {
-            const screenshot = screenshots[0]; // Get the first screenshot from the result
-
-            // Create a readable stream from the screenshot buffer
-            const readStream = new stream.PassThrough();
-            readStream.end(screenshot);
-
-            // Set the response headers and send the screenshot
+            const buffer = sharp(screenshots[0]);
+            if(grayscale) {
+                buffer
+                .grayscale()
+                .toFormat('png', { bitdepth: 8 });
+            }
+            if(negate) {
+                buffer.negate();
+            }
+            const processedBuffer = await buffer.toBuffer();
+            res.setHeader('Content-Length', processedBuffer.length);
             res.setHeader('Content-Type', 'image/png');
             res.setHeader('Content-Disposition', 'inline; filename="screenshot.png"');
-            readStream.pipe(res);
+
+            res.end(processedBuffer);
         } else {
             res.status(500).send('No screenshot captured.');
         }
@@ -44,6 +57,7 @@ app.get('/screenshot', async (req, res) => {
         console.error('Error capturing screenshot:', error);
         res.status(500).send('Failed to capture screenshot.');
     }
+    console.log();
 });
 
 app.listen(PORT, () => {
